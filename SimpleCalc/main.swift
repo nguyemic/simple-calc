8c7bfb543ebe9ac98e5c3599248bfdbf03b95137
Michael Nguyen <nguyemic@uw.edu>
2017-04-02 23:08:29 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Michael Nguyen on 4/2/17.
+//  Copyright © 2017 Michael Nguyen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
